ac1fee01adabc670bc2f03470540f2008aa46243
akoyiwilliam79@gmail.com <akoyiwilliam79@gmail.com>
2026-02-17 14:41:00 +0300

    new commit

---
+++ functions.py
@@ -0,0 +1,49 @@
+#functions - is a block of code used to perform a task
+#types
+#1. standard library/ inbuilt functions
+#2. user defined - are functions created by the user
+
+#inbuilt functions - already exist
+
+y = max(45,89,34,300,678,432,78)
+print("The maximum number is",y)
+
+x = min(45,98,786,98,9,3,2,5)
+print("The minimum number is",x)
+
+
+#2. User defined functions - created by users
+#def is a key word used when creating functions
+
+def school():
+    print("Welcome to emobilis")
+
+
+school()
+
+print()
+
+#parameters/variables
+#arguements/values
+def add(num1,num2):#parameters
+
+    print("The sum is",num1+num2)
+
+add(23,34)#Arguements
+add(34,90)
+add(78,98)
+
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
--- loops.py
+++ loops.py
@@ -4,10 +4,32 @@ while number <=10:
     print(number)
     number += 1
 
-
+print()
 while number <= 35:
     print(number)
     number += 1
+
+print()
+#program 2
+
+count = 105
+while count >= 100:
+    print("Number is",count)
+    count -= 1
+
+print()
+#for loop
+for num in range(1, 6 ,2):
+    print("Number:",num)
+
+#Break and continue statement(research on it)
+
+
+
+
+
+
+
 
 
 
